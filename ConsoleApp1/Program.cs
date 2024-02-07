ec868c58992bb502a38af3af512b0aed9a980e27
Bailey Pollard <baipoll@chbc.school.nz>
2024-02-07 14:16:22 +1300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+   
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Paper, scissors, rock");
+
+           for(int i = 1; i < 4; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
